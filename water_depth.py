30f8545d713d89c95c1a3e1dc4df898a403b6960
Leon Windheuser <leon.windheuser@gmail.com>
2021-05-27 14:55:47 +0200

    Use generators for csv labels

--- water_depth.py
+++ water_depth.py
@@ -1,35 +1,89 @@
+import os
+import numpy as np
+import pandas as pd
+
 import tensorflow as tf
 from tensorflow.keras import models, Model, layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 import matplotlib.pyplot as plt
 
 
+def append_ext(fn):
+    """Append the extensions to the .csv labels"""
+    return fn + ".png"
+
+
 def load_dataset():
-    return (), ()
+    dataset_path = "./datasets/RockyCreek/"
 
+    traindf = pd.read_csv(dataset_path + "labels.csv", dtype=str, sep=", ")
+    print(traindf)
+    testdf = pd.read_csv(dataset_path + "labels.csv", dtype=str, sep=", ")
 
-def create_conv_base() -> Model:
-    model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)))
-    model.add(layers.MaxPool2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D(2, 2))
-    model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-    return model
+    traindf["time"] = traindf["time"].apply(append_ext)
+    testdf["time"] = testdf["time"].apply(append_ext)
 
+    datagen = ImageDataGenerator(rescale=1. / 255., validation_split=0.2)
 
-def add_dense_layers(model: Model) -> Model:
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation="relu"))
-    model.add(layers.Dense(10))
-    return model
+    train_generator = datagen.flow_from_dataframe(
+        dataframe=traindf,
+        directory=dataset_path,
+        x_col="time",
+        y_col="height",
+        subset="training",
+        batch_size=32,
+        shuffle=True,
+        class_mode="sparse",
+        target_size=(800, 450, 3)
+    )
+
+    valid_generator = datagen.flow_from_dataframe(
+        dataframe=traindf,
+        directory=dataset_path,
+        x_col="time",
+        y_col="height",
+        subset="validation",
+        batch_size=32,
+        shuffle=True,
+        class_mode="sparse",
+        target_size=(800, 450, 3)
+    )
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255.)
+    test_generator = test_datagen.flow_from_dataframe(
+        dataframe=testdf,
+        directory=dataset_path,
+        x_col="time",
+        y_col="height",
+        batch_size=32,
+        shuffle=False,
+        class_mode=None,
+        target_size=(800, 450, 3)
+    )
+
+    return train_generator, valid_generator, test_generator
 
 
 def create_model() -> Model:
-    model = add_dense_layers(create_conv_base())
-    model.compile(optimizer="adam",
-                  loss=tf.keras.losses.MeanSquaredError,
-                  metrics=["accuracy"])
-    return model
+    cnn = models.Sequential()
+    cnn.add(layers.Conv2D(32, 3, 3, activation="relu", input_shape=(800, 450, 3)))
+    cnn.add(layers.MaxPool2D((2, 2)))
+    cnn.add(layers.Conv2D(64, 3, 3, activation="relu"))
+    cnn.add(layers.MaxPooling2D(2, 2))
+    cnn.add(layers.Conv2D(64, 3, 3, activation="relu"))
+
+    cnn.add(layers.Flatten())
+    cnn.add(layers.Dense(64, activation="relu"))
+    cnn.add(layers.Dense(10))
+
+    cnn.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.MeanSquaredError,
+        metrics=["accuracy"]
+    )
+
+    return cnn
 
 
 def plot_model(history):
@@ -37,10 +91,21 @@ def plot_model(history):
 
 
 if __name__ == "__main__":
-    (train_images, train_labels), (test_images, test_labels) = load_dataset()
+    train_gen, valid_gen, test_gen = load_dataset()
 
     model: Model = create_model()
-    history = model.fit(train_images, train_labels, epochs=10,
-                        validation_data=(test_images, test_labels))
+    model.summary()
+
+    STEP_SIZE_TRAIN = train_gen.n // train_gen.batch_size
+    STEP_SIZE_VALID = valid_gen.n // valid_gen.batch_size
+    STEP_SIZE_TEST = test_gen.n // test_gen.batch_size
+
+    history = model.fit_generator(
+        generator=train_gen,
+        steps_per_epoch=STEP_SIZE_TRAIN,
+        validation_data=valid_gen,
+        validation_steps=STEP_SIZE_VALID,
+        epochs=10
+    )
 
     plot_model(history)
