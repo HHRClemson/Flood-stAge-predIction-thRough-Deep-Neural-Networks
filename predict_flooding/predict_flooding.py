d1cca2f2ec48428322feb59dbfc1a3a80d7bcdda
Leon Windheuser <leon.windheuser@gmail.com>
2021-08-17 00:37:32 +0200

    Add logging and minor fixes

--- predict_flooding/predict_flooding.py
+++ predict_flooding/predict_flooding.py
@@ -9,19 +9,19 @@ from predict_flooding.window_generator import SlidingWindowGenerator
 from predict_flooding.models import *
 
 OUT_STEPS = 10
-NUM_EPOCHS = 100
+NUM_EPOCHS = 50
 
 
-def evaulate_baseline(_, test_ds):
+def _evaluate_baseline(_, test_ds):
     # since the baseline model does not predict anything, we do not need the training ds
-    model: Model = baseline.Baseline()
+    model: Model = baseline.Baseline(label_index=1)
     model.compile(loss=tf.losses.MeanSquaredError(),
                   metrics=[tf.metrics.MeanAbsoluteError()])
     performance = model.evaluate(test_ds)
     return performance
 
 
-def evaluate_dense(train_ds, test_ds):
+def _evaluate_dense(train_ds, test_ds):
     model: Model = dense.Dense()
     model.compile(loss=tf.losses.MeanSquaredError(),
                   metrics=[tf.metrics.MeanAbsoluteError()])
@@ -30,7 +30,7 @@ def evaluate_dense(train_ds, test_ds):
     return performance
 
 
-def evaluate_cnn(train_ds, test_ds):
+def _evaluate_cnn(train_ds, test_ds):
     model: Model = cnn.CNN(out_steps=OUT_STEPS)
     model.compile(loss=tf.losses.MeanSquaredError(),
                   metrics=[tf.metrics.MeanAbsoluteError()])
@@ -39,7 +39,7 @@ def evaluate_cnn(train_ds, test_ds):
     return performance
 
 
-def evaluate_lstm(train_ds, test_ds):
+def _evaluate_lstm(train_ds, test_ds):
     model: Model = lstm.LSTM()
     model.compile(loss=tf.losses.MeanSquaredError(),
                   metrics=[tf.metrics.MeanAbsoluteError()])
@@ -50,29 +50,45 @@ def evaluate_lstm(train_ds, test_ds):
 
 def train_and_predict(path):
     df = pd.read_csv(path)
+    df = df.drop("time", axis=1)
+    df["height"] = pd.to_numeric(df["height"], errors="coerce")
+    df["perception"] = pd.to_numeric(df["perception"], errors="coerce")
+
     train_test_split = int(len(df) * 0.8)
     train_df = df[0:train_test_split]
     test_df = df[train_test_split:]
-    #print(train_df.head())
 
-    """Normalize the data by using the training mean and standard deviation."""
+    # Normalize the data by using the training mean and standard deviation.
     train_mean = train_df.mean()
     train_std = train_df.std()
+    print(train_mean)
+    print(train_std)
 
     train_df = (train_df - train_mean) / train_std
     test_df = (test_df - train_mean) / train_std
 
     window_generator: SlidingWindowGenerator = SlidingWindowGenerator(
         input_width=100, label_width=1, shift=OUT_STEPS,
-        train_df=train_df, test_df=test_df)
+        train_df=train_df, test_df=test_df,
+        label_columns=["height"])
+    print(window_generator)
 
     train_ds = window_generator.train_dataset
+    print(train_ds)
     test_ds = window_generator.test_dataset
+    print(test_ds)
 
-    baseline_performance = evaulate_baseline(train_ds, test_ds)
-    dense_performance = evaluate_dense(train_ds, test_ds)
-    cnn_performance = evaluate_cnn(train_ds, test_ds)
-    lstm_performance = evaluate_lstm(train_ds, test_ds)
+    baseline_performance = _evaluate_baseline(train_ds, test_ds)
+    print("EVALUATED BASELINE:", baseline_performance)
+
+    dense_performance = _evaluate_dense(train_ds, test_ds)
+    print("EVALUATED DENSE:", dense_performance)
+
+    cnn_performance = _evaluate_cnn(train_ds, test_ds)
+    print("EVALUATED CNN:", cnn_performance)
+
+    lstm_performance = _evaluate_lstm(train_ds, test_ds)
+    print("EVALUATED LSTM:", lstm_performance)
 
     print("\n".join([
         "Baseline performance: {}".format(baseline_performance),
